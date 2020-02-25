3e227c6a070bab635f0dea07cd518672a8d3876a
Mason Ellwood <masonellwood@gmail.com>
2020-02-25 16:48:28 -0700

    curremt

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color(0xff090B22),
-        accentColor: Colors.purple,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        accentColor: Colors.pink,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+            body1: TextStyle(
+          color: Color(0xFFFFFFFF),
+        )),
       ),
       home: InputPage(),
     );
