972bde0fcd36884545c30e883e527075b8e7d809
Mason Ellwood <masonellwood@gmail.com>
2020-02-25 19:16:32 -0700

    added colors to widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,14 +12,60 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        height: 200.0,
-        width: 170.0,
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              color: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({@required this.color});
+  
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
